616180aaea8253a41425f79889a34894f995ff8f
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-10-15 12:38:53 +0200

    modificaciones

--- Matriz_3X3.java
+++ Matriz_3X3.java
@@ -1,9 +1,10 @@
 public class Matriz_3X3 {
     public static void main(String[] args) {
-        int[] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         
         for (int i = 0; i < matriz.length; i++) {
-        System.out.print(matriz[i] + " ");
+        for (int j = 0; j < matriz[i].length; j++) {
+        System.out.print(matriz[i][j] + " ");
         }
         System.out.println();
         }
--- Matriz_Transpuesto.java
+++ Matriz_Transpuesto.java
@@ -1,3 +1,23 @@
+import java.util.Arrays; 
 public class Matriz_Transpuesto {
-    
+    public static void main(String[] args) {
+
+        int[][] matrizOriginal = {
+            {1, 2, 3},
+            {4, 5, 6} 
+        };
+        int filasOriginal = matrizOriginal.length;
+        int columnasOriginal = matrizOriginal[0].length;
+        int[][] matrizTranspuesta = new int[columnasOriginal][filasOriginal];
+        for(int i = 0; i < filasOriginal; i++) { 
+            for(int j = 0; j < columnasOriginal; j++) { 
+                matrizTranspuesta[j][i] = matrizOriginal[i][j];
+            }
+        }
+        System.out.println("\nMatriz transpuesta:"); 
+        for(int i = 0; i < columnasOriginal; i++) { 
+            System.out.println(Arrays.toString(matrizTranspuesta[i]));
+    }
+    }
 }
+
