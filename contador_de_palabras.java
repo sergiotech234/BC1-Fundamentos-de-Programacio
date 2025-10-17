f053585205bc566b41483edf2d293cc42b7f744c
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-10-17 10:00:53 +0200

    añadadimos comentario

--- contador_de_palabras.java
+++ contador_de_palabras.java
@@ -5,11 +5,12 @@ public class contador_de_palabras {
         return palabras.length;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         System.out.print("Ingrese una cadena de texto: ");
-        String texto = sc.nextLine();
+        //Para que cuente palabras, necesitaremos una variable string e int para que se dividan en bloques y se puedan contar los caracteres
+        String texto = entrada.nextLine();
         int numPalabras = contarPalabras(texto);
         System.out.println("La cadena tiene " + numPalabras + " palabras.");
-        sc.close();
+        entrada.close();
     }
 }
