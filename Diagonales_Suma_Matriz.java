8812e844d5ab48810896ec2a3d4f3ef661d40fd5
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-10-17 09:27:54 +0200

    Modificaciones

--- Diagonales_Suma_Matriz.java
+++ Diagonales_Suma_Matriz.java
@@ -7,31 +7,56 @@ public class Diagonales_Suma_Matriz {
             {7, 8, 9}
         };
 
-        // Imprimir matriz
-        System.out.println("Matriz:");
+        // Imprimir matriz original
+        System.out.println("Matriz original:");
+        imprimirMatriz(matriz);
+
+        // Sumar diagonal principal
+        int sumaPrincipal = sumarDiagonalPrincipal(matriz);
+        System.out.println("\nLa suma de la diagonal principal es: " + sumaPrincipal);
+
+        invertirDiagonalSecundaria(matriz);
+        // Sumar diagonal secundaria después de invertir
+        int sumaSecundariaInvertida = sumarDiagonalSecundaria(matriz);
+        System.out.println("La suma de la diagonal secundaria invertida es: " + sumaSecundariaInvertida);
+    }
+
+    // Método para sumar la diagonal principal
+    public static int sumarDiagonalPrincipal(int[][] matriz) {
+        int suma = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            suma += matriz[i][i];
+        }
+        return suma;
+    }
+
+    // Método para sumar la diagonal secundaria
+    public static int sumarDiagonalSecundaria(int[][] matriz) {
+        int suma = 0;
+        int n = matriz.length;
+        for (int i = 0; i < n; i++) {
+            suma += matriz[i][n - 1 - i];
+        }
+        return suma;
+    }
+
+    // Método para invertir la diagonal secundaria
+    public static void invertirDiagonalSecundaria(int[][] matriz) {
+        int n = matriz.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = matriz[i][n - 1 - i];
+            matriz[i][n - 1 - i] = matriz[n - 1 - i][i];
+            matriz[n - 1 - i][i] = temp;
+        }
+    }
+
+    // Método para imprimir una matriz
+    public static void imprimirMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
         }
-
-        // Sumar todos los elementos
-        int resultado = sumarElementosArrays(matriz);
-        System.out.println("La suma de todos los elementos es: " + resultado);
-    }
-
-    // Método para sumar todos los elementos de una matriz 2D
-    public static int sumarElementosArrays(int[][] matriz) {
-        int suma = 0;
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if(i==j){
-                    suma += matriz[i][j];
-                }
-            }
-        }
-        return suma;
-        //correccion de codigo
     }
 }
